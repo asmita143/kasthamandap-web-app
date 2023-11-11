f9c5e19d61f0b656c477a92a3c30e0096e09a5f6
Bibek <study.bibekshrestha@gmail.com>
2023-11-12 00:45:18 +0200

    deploy

--- kasthamandap/vite.config.js
+++ kasthamandap/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kasthamandap/',
+  base: '/kasthamandap-web-app/',
 });
